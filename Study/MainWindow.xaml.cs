cbe6f23078881c7b962369b1d9800b9cb530b315
Анна Клещенко <aakleschenko@edu.hse.ru>
2019-12-15 00:24:18 +0300

    Пытаюсь синхронизировать с базой данных. Создала бд на локальном серваке своего компа пока, хз, будет ли у вас к ней доступ. ЗАПРОС НЕ ЛОМАТЬ

--- Study/MainWindow.xaml.cs
+++ Study/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data.SqlClient;
 namespace Study
 {
     /// <summary>
@@ -20,13 +20,32 @@ namespace Study
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+    
         public MainWindow()
         {
             InitializeComponent();
+            
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            using (SqlConnection connection = new SqlConnection("Data Source = (local)\\SQLEXPRESS; Initial Catalog = UsersDatabaseKDZ; Integrated Security = True; Pooling = False"))
+            {
+                string queryString = "SELECT login FROM Users WHERE Login=\'" + LoginTextBox.Text + "\' and Password=\'" + PasswordTextBox.Password + "\';";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader == "")
+                {
+                    MessageBox.Show("Oops! There's no user with such login&password.");
+                    queryString = "SELECT * FROM Users WHERE Login=\'" + LoginTextBox.Text + "\' and Password=\'" + PasswordTextBox.Password + "\';";
+                    command = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    reader = command.ExecuteReader();
+                }
+                reader.Close();
 
+            }
             var choic = new ChoiceWindow();
             choic.ShowDialog();
 
