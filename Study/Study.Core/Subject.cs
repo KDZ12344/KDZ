759a31a5cf39bd1be883b572567b29aeb2a96d7a
lenavolkovs <58178482+lenavolkovs@users.noreply.github.com>
2019-12-15 00:21:55 +0300

    шлю всем (только ане) классы!!!

---
+++ Study/Study.Core/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Study.Core
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+        public List<Interest> Interests { get; set; }
+
+        public Subject()
+        {
+        }
+    }
+}
