b5bf653c4c1acb620956c12b03804261ebad6660
lenavolkovs <58178482+lenavolkovs@users.noreply.github.com>
2019-12-15 14:29:01 +0300

    переименовала в Study.Core

---
+++ Core/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.SqlClient;
+
+namespace Study.Core
+{
+    public class Repository
+    {
+        public List<User> Users { get; set; }
+        public List<Interest> Interests { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        List<User> users = new List<User>();
+
+        public void Registration(string name, string login, string password, DateTime birthDate, string? vk, string? telegram)
+        {
+            int id = users.Count;
+            User user = new User
+            {
+                Name = name,
+                Login = login,
+                UserId = id,
+                Password = password,
+                BirthDate = birthDate,
+                DateAdded = DateTime.Now,
+                VKID = vk,
+                TelegramID = telegram
+                
+            };
+
+        }
+    }
+}
--- Study/MainWindow.xaml.cs
+++ Study/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
-using Core;
+using Study.Core;
 namespace Study
 {
     public partial class MainWindow : Window
